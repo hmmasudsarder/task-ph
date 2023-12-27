c075a13613a38dd773bf4a281410371e5d5b5663
hm masud sarder <hmmasudsarder1.com>
2023-12-27 05:40:35 -0800

    task management coulm added

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,16 +2,18 @@ import { useState } from "react"
 import CreateTask from "./CreateTask"
 import ListTask from "./ListTask";
 import Navbar from "../Home/Navbar/Navbar";
+import Footer from "../Home/Footer";
 
 const Dashboard = () => {
-    const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
   return (
     <div className="">
-      <Navbar/>
+      <Navbar />
       <div className="bg-slate-100 w-screen h-screen flex flex-col items-center pt-4 gap-14">
-      <CreateTask tasks={tasks} setTasks={setTasks}/>
-      <ListTask tasks={tasks} setTasks={setTasks} />
-    </div>
+        <CreateTask tasks={tasks} setTasks={setTasks} />
+        <ListTask tasks={tasks} setTasks={setTasks} />
+      </div>
+      <Footer />
     </div>
   )
 }
--- src/Pages/Dashboard/ListTask.jsx
+++ src/Pages/Dashboard/ListTask.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const ListTask = ({tasks, setTasks}) => {
   return (
-    <div>
-      
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20'>
+      <div className="w-64 h-96 shadow-xl flex justify-center rounded-lg">
+        <div className="bg-sky-500 w-60 h-14 flex items-center justify-center rounded-lg">
+          <h1 className='text-white'>TODO LIST</h1>
+        </div>
+      </div>
+      <div className="w-64 h-96 shadow-xl flex justify-center rounded-lg">
+        <div className="bg-sky-500 w-60 h-14 flex items-center justify-center rounded-lg">
+          <h1 className='text-white uppercase'>ongoing</h1>
+        </div>
+      </div>
+      <div className="w-64 h-96 shadow-xl flex justify-center rounded-lg">
+        <div className="bg-sky-500 w-60 h-14 flex items-center justify-center rounded-lg">
+          <h1 className='text-white uppercase'>completed</h1>
+        </div>
+      </div>
     </div>
   )
 }
